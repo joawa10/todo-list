47c2e872aca761399962f38941247f25a94d1b65
joawa10 <joawa10@yahoo.com>
2025-02-15 18:39:42 +0800

    refactor frontend structure: implement todo management with CRUD operations and update main app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,89 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Todo from './components/Todo';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const BASE_URL = "http://localhost:4001/api";
 
 function App() {
-  const headStyle = {
-    textAlign: "center",
-  }
+  const [todos, setTodos] = useState(null);
+  const [todo, setTodo] = useState("");
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = () => {
+    axios
+      .get(`${BASE_URL}/todos`)
+      .then((res) => setTodos(res.data))
+      .catch((err) => console.error(err));
+  };
+
+  const handleAddTodo = () => {
+    axios
+      .post(`${BASE_URL}/todo/new`, {
+        title: todo,
+      })
+      .then((res) => {
+        setTodos([...todos, res.data]);
+        setTodo("");
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleDeleteTodo = (id) => {
+    axios
+    .delete(`${BASE_URL}/todo/delete/${id}`)
+    .then((res) =>
+      setTodos(todos.filter((todo) => todo._id !== res.data._id))
+    )
+    .catch((err) =>
+        console.error(err)
+      );
+  };
+
+  const handleTodoClick = (id) => {
+    axios
+      .get(`${BASE_URL}/todo/toggleStatus/${id}`)
+      .then((res) => getTodos())
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <div>
-      <h1 style={headStyle}>Todo List</h1>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Todo/>}></Route>
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <div className="todo-input-wrapper">
+        <input
+          className="todo-input-bar"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Add a New Todo"
+        />
+        <div className="add-button" onClick={handleAddTodo}>
+          <button>+</button>
+        </div>
+      </div>
+      <div className="todos-list">
+        {!todos || !todos.length ? (
+          <h3 style={{ textAlign: "center" }}>No Todo Data !!!</h3>
+        ) : (
+          todos.map((todo) => (
+            <div className="todo" key={todo._id}>
+              <div
+                onClick={() => handleTodoClick(todo._id)}
+                className={todo.complete ? "complete" : ""}
+                id="todo-title"
+              >
+                {todo.title}
+              </div>
+              <div
+                className="delete"
+                onClick={() => handleDeleteTodo(todo._id)}
+              >
+                <button>X</button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
