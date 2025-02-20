cba29f7cfde4901cfc8b13e284e77088b164dcd9
joawa10 <joawa10@yahoo.com>
2025-02-20 22:50:45 +0800

    setup a local postgresql db and connect there

--- backend_prisma/hello-prisma/src/index.ts
+++ backend_prisma/hello-prisma/src/index.ts
@@ -12,5 +12,7 @@ app.use(cors());
 
 app.use('/api', todoRoute);
 
+console.log("Connected to database:", process.env.DATABASE_URL); // Log the connection URL
+
 const port = process.env.PORT || 4001;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
